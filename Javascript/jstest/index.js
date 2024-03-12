825db7100ffd10c121cc366f8dad050186e9a0ce
Sujon <57975469+SujonHiro@users.noreply.github.com>
2024-03-12 22:04:39 +0600

    function compute the absolute value

--- Javascript/jstest/index.js
+++ Javascript/jstest/index.js
@@ -1,12 +1,21 @@
-let y=5;
-function plus1(x){
-    return x+1
+// let y=5;
+// function plus1(x){
+//     return x+1
+// }
+
+// plus1(y);
+
+
+// const square=function(x){
+// }
+// console.log(square(plus1(y)))
+
+
+function abs(x){
+    if(x>=0){
+        return x;
+    }else{
+        return -x;
+    }
 }
-
-plus1(y);
-
-
-const square=function(x){
-    return x*x
-}
-console.log(square(plus1(y)))
+console.log(abs(-10)===abs(10))
