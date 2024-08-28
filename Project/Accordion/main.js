e90df14ad2a411f0ac570f58c11a5325d99d106f
Sujon <57975469+SujonHiro@users.noreply.github.com>
2024-08-28 16:52:06 +0600

    another way of accordion

--- Project/Accordion/main.js
+++ Project/Accordion/main.js
@@ -9,18 +9,18 @@ function displayAccordionData() {
     const accordionAnswerDiv = document.createElement("div");
     const answerText = document.createElement("p");
     const quesionText = document.createElement("h2");
-    const quesionIcon=document.createElement("i")
+    const quesionIcon = document.createElement("i");
 
-    quesionIcon.classList.add("fa-solid", "fa-arrow-up")
+    quesionIcon.classList.add("fa-solid", "fa-arrow-up");
     accordionDiv.classList.add("accordion-main");
     accordionQuestionDiv.classList.add("accordion-question");
     accordionAnswerDiv.classList.add("accordion-answer");
-   
+
     quesionText.textContent = item.question;
     answerText.textContent = item.answer;
 
     accordionQuestionDiv.appendChild(quesionText);
-    accordionQuestionDiv.appendChild(quesionIcon)
+    accordionQuestionDiv.appendChild(quesionIcon);
     accordionAnswerDiv.appendChild(answerText);
     accordionDiv.appendChild(accordionQuestionDiv);
     accordionDiv.appendChild(accordionAnswerDiv);
@@ -29,7 +29,8 @@ function displayAccordionData() {
   accordionFunctionaly();
 }
 
-function accordionFunctionaly() {
+//step-1
+/* function accordionFunctionaly() {
   const quesions = document.querySelectorAll(".accordion-question");
 
   quesions.forEach((quesionBtn) => {
@@ -43,6 +44,29 @@ function accordionFunctionaly() {
         });
         quesionBtn.classList.add("active");
       }
+    });
+  });
+} */
+
+function accordionFunctionaly() {
+  const questions = document.querySelectorAll(".accordion-main");
+
+  questions.forEach((question) => {
+    //console.log(question);
+    const quesBtn = question.querySelector(".accordion-question");
+    //console.log(quesBtn);
+    
+    quesBtn.addEventListener("click", () => {
+      questions.forEach((item) => {
+        //console.log(item);
+
+        if (item !== question) {
+
+          item.classList.remove("show-text")
+        }else{
+          item.classList.toggle("show-text")
+        }
+      });
     });
   });
 }
